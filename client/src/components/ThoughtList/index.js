d8420aabf06f217de2f991284caf21c76f573f65
WinnieCWu <winniechn7@gmail.com>
2022-05-16 11:30:15 -0700

    implemented nav links and url parameters, then built single page thought

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-
-//receive 2 props: title and thoughts array 
+import React from "react";
+import { Link } from "react-router-dom";
+//receive 2 props: title and thoughts array
 //destructured props to avoid needing to use props.title and props.thoughts throughout JSX code
 const ThoughtList = ({ thoughts, title }) => {
   if (!thoughts.length) {
@@ -11,18 +11,26 @@ const ThoughtList = ({ thoughts, title }) => {
     <div>
       <h3>{title}</h3>
       {thoughts &&
-        thoughts.map(thought => (
+        thoughts.map((thought) => (
           <div key={thought._id} className="card mb-3">
             <p className="card-header">
-              {thought.username}
+              <Link
+                to={`/profile/${thought.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {thought.username}
+              </Link>{" "}
               thought on {thought.createdAt}
             </p>
             <div className="card-body">
-              <p>{thought.thoughtText}</p>
-              <p className="mb-0">
-                Reactions: {thought.reactionCount} || Click to{' '}
-                {thought.reactionCount ? 'see' : 'start'} the discussion!
-              </p>
+              <Link to={`/thought/${thought._id}`}>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionCount} || Click to{" "}
+                  {thought.reactionCount ? "see" : "start"} the discussion!
+                </p>
+              </Link>
             </div>
           </div>
         ))}
